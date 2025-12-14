66c2b93bf916c884f18f6d7b1ade45ac2708bab7
Brandon Coleman <brandon.c.coleman84@gmail.com>
2025-12-14 01:59:24 -0600

    Time: 235 ms (96.19%), Space: 67.5 MB (25.61%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    #drop duplicates from dataframe sort in descending order and reset index
+    salaries = employee['salary'].drop_duplicates().sort_values(ascending=False).reset_index(drop=True)
+
+    print(salaries)
+
+    # base case if number of distinct salaries is less than N return null
+    if(len(salaries) < 2 ) : return pd.DataFrame({"SecondHighestSalary": [None]})
+
+    return pd.DataFrame({"SecondHighestSalary": [salaries.iloc[1]]})
+    
